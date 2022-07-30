d072ab49ceb638a85d6a4e987eed9660d25d2a89
vcho1958 <gusckd57@gmail.com>
2022-07-30 13:03:37 +0900

    [create]CustomException class with ErrorCode enum

---
+++ src/main/java/com/example/groom/common/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.groom.common.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    ACCESS_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, "액세스 토큰이 필요한 요청입니다."),
+    REFRESH_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, "리프레쉬 토큰이 필요한 요청입니다."),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "리프레쉬 토큰이 만료되었습니다."),
+    TOKEN_VALIDATE_FAIL(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰 입니다.")
+    ;
+    private final HttpStatus status;
+    private final String message;
+}
