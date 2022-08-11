7962dd73d95291e07c218abe19cd50187cb0fc3c
이진원 <jin-won@ijin-won-ui-MacBookAir.local>
2022-08-11 18:15:05 +0900

    Feat: Add column

---
+++ src/main/java/com/example/groom/entity/FileEntity.java
@@ -0,0 +1,2 @@
+package com.example.groom.entity;public class FileEntity {
+}
---
+++ src/main/java/com/example/groom/entity/TodoAttatchmentEntity.java
@@ -0,0 +1,6 @@
+package com.example.groom.entity.todo;
+
+import com.example.groom.entity.FileEntity;
+
+public class TodoAttatchmentEntity extends FileEntity {
+}
