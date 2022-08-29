09e5949b1424c3d92d93e38c77c02e073d71ee37
vcho1958 <gusckd57@gmail.com>
2022-08-30 06:01:27 +0900

    [feat] ModelMapper Bean 생성

--- src/main/java/com/example/groom/domain/auth/RefreshToken/RefreshTokenRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.example.groom.domain.auth.RefreshToken;
-
-import com.example.groom.entity.RefreshToken;
-
-public interface RefreshTokenRepositoryCustom {
-    RefreshToken getRefreshTokenFromDB(String refreshToken);
-
-}
