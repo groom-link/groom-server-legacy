7c3dbf2bd8d3cb7df544291bd3f52313c8c9ce9f
vcho1958 <gusckd57@gmail.com>
2022-09-23 01:41:48 +0900

    [setting]cors

--- src/main/java/com/example/groom/WebConfig.java
+++ src/main/java/com/example/groom/WebConfig.java
@@ -4,6 +4,8 @@ package com.example.groom;
 import com.example.groom.common.interceptor.ResponseInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(responseInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.POST.name())
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
