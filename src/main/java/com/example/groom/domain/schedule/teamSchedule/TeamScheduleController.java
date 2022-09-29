79e3205815450cf2fe4524f0bf31a782bb655a6f
이진원 <jin-won@ijin-won-ui-MacBookAir.local>
2022-09-29 19:47:28 +0900

    fix: 팀 스케줄의 api 엔드포인트에 roomId가 들어가도록 통일함

--- src/main/java/com/example/groom/domain/schedule/teamSchedule/TeamScheduleController.java
+++ src/main/java/com/example/groom/domain/schedule/teamSchedule/TeamScheduleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/team-schedule")
+@RequestMapping("/room/{roomId}/schedule")
 public class TeamScheduleController {
 
     private final TeamScheduleService teamScheduleService;
