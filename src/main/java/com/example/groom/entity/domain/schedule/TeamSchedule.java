9135d3d963744ea2e076a685de30f0bc1b7e9bbb
이진원 <jin-won@ijinwoncBookAir.kornet>
2022-10-05 03:38:44 +0900

    fix: 팀 스케줄 룸 삭제 룸 삭제 후 유저 아이디를 서치 컨디션에 통합, 중간테이블 생성 이전까지 작업

--- src/main/java/com/example/groom/entity/domain/schedule/TeamSchedule.java
+++ src/main/java/com/example/groom/entity/domain/schedule/TeamSchedule.java
@@ -1,12 +1,13 @@
 package com.example.groom.entity.domain.schedule;
 
 import com.example.groom.domain.schedule.teamSchedule.dto.TeamScheduleDto;
-import com.example.groom.entity.domain.room.Room;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
 
 @Getter
 @Entity
@@ -17,13 +18,8 @@ public class TeamSchedule extends Schedule {
     @Embedded
     private MeetingLocation meetingLocation;
 
-    @JoinColumn
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Room room;
-
     protected TeamSchedule(TeamScheduleDto teamScheduleDto) {
         this.meetingLocation = teamScheduleDto.getMeetingLocation();
-        this.room = teamScheduleDto.getRoom();
     }
 
     static public TeamSchedule of(TeamScheduleDto teamScheduleDto) {
